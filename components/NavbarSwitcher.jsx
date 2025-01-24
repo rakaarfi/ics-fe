ce852306c2f985df420bd369e7d2e7d7536bf918
rakaarfi <rakaarfi@gmail.com>
2025-01-24 10:50:53 +0700

    add IAPNavbar

--- components/NavbarSwitcher.jsx
+++ components/NavbarSwitcher.jsx
@@ -1,13 +1,16 @@
-// components/NavbarSwitcher.jsx
 "use client";
 
 import { usePathname } from "next/navigation";
 import { Navbar } from "@/components/Navbar";
 import { InputNavbar } from "@/components/ImtMembers/InputNavbar";
+import { IAPNavbar } from "./IAP/IAPNavbar";
 
 export default function NavbarSwitcher() {
     const pathname = usePathname();
     const showInputNavbar = pathname.startsWith("/dashboard/input-imt-members");
+    const showIAPNavbar = pathname.startsWith("/dashboard/iap");
 
-    return showInputNavbar ? <InputNavbar /> : <Navbar />;
+    if (showIAPNavbar) return <IAPNavbar />;
+    if (showInputNavbar) return <InputNavbar />;
+    return <Navbar />;
 }
