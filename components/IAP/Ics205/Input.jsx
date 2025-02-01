acb85db229df2043b782d6eadb5aec7bdec2f439
rakaarfi <rakaarfi@gmail.com>
2025-02-01 22:58:50 +0700

    add ics-205 components

--- components/IAP/Ics205/Input.jsx
+++ components/IAP/Ics205/Input.jsx
@@ -2,41 +2,133 @@
 
 import { ButtonSubmit } from '@/components/ButtonComponents';
 import FormContainer from '@/components/FormContainer';
-import React from 'react'
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
 
 export default function Input() {
+    const [formData, setFormData] = useState({
+        operational_period_id: null,
+        special_instructions: "",
+        radioChannel: [
+            {
+                channel_number: "",
+                channel_name: "",
+                type_specification: "",
+                frequency: "",
+                mode: "",
+                functions: "",
+                assignment: "",
+                remarks: "",
+            },
+        ],
+    });
+    const [incidentData, setIncidentData] = useState([]);
+    const [operationalPeriodData, setOperationalPeriodData] = useState([]);
+    const [CULeaderData, setCULeaderData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const handleIncidentChange = (e) => {
+        const incident_id = parseInt(e.target.value, 10);
+        if (!incident_id) return;
+
+        setLoading(true);
+        setError(null);
+        setOperationalPeriodData([]);
+        setFormData((prevState) => ({
+            ...prevState,
+            incident_id,
+            operational_period_id: "",
+        }));
+
+        axios.get(`http://127.0.0.1:8000/operational-period/read-by-incident/${incident_id}`)
+            .then((response) => {
+                setOperationalPeriodData(response.data);
+            })
+            .catch(() => setError('Failed to fetch operational period data'))
+            .finally(() => setLoading(false));
+    };
+
+
+    const handleOperationalPeriodChange = (e) => {
+        const operational_period_id = parseInt(e.target.value, 10);
+        setFormData(prevState => ({
+            ...prevState,
+            operational_period_id
+        }));
+    };
+
+    const handleChange = (e) => {
+        const { name, value, type, checked } = e.target;
+        setFormData({
+            ...formData,
+            [name]: type === "checkbox" ? checked : value,
+        });
+    };
+
+    const fetchIncidentData = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/incident-data/read');
+            setIncidentData(response.data);
+        } catch (error) {
+            console.error('Error fetching incident data:', error);
+            setError('Failed to fetch incident data');
+        }
+    };
+
+    useEffect(() => {
+        fetchIncidentData();
+    }, []);
+
+    const fetchCULeader = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/logistic-section/communication-unit-leader/read/');
+            setCULeaderData(response.data);
+            console.log("Communication Unit Leader Data:", response.data);
+        } catch (error) {
+            console.error('Error fetching Communication Unit Leader data:', error);
+            setError('Failed to fetch Communication Unit Leader data');
+        }
+    };
+
+    useEffect(() => {
+        fetchCULeader();
+    }, []);
+
     return (
         <FormContainer title="Input ICS 205" >
             <div className="mb-4 flex flex-row">
                 <select
-                    name="incident_id"
                     className="flex-1 block w-[400px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
-                // onChange={(e) => handleIncidentChange(parseInt(e.target.value, 10))}
-                // value={formData.incident_id || ""}
+                    value={formData.incident_id || ""}
+                    onChange={handleIncidentChange}
+                    required
                 >
-                    <option value={""} disabled>
+                    <option value="" disabled>
                         Select Incident
                     </option>
-                    {/* {incidentData.map((incident) => (
+                    {incidentData.map((incident) => (
                         <option key={incident.id} value={incident.id}>
                             {incident.name}
                         </option>
-                    ))} */}
+                    ))}
                 </select>
+
                 <select
-                    name="id"
                     className="flex-1 block w-[400px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
-                // onChange={(e) => handleIncidentChange(parseInt(e.target.value, 10))}
-                // value={formData.incident_id || ""}
+                    value={formData.operational_period_id || ""}
+                    onChange={handleOperationalPeriodChange}
+                    disabled={loading || !formData.incident_id}
+                    required
                 >
-                    <option value={""} disabled>
-                        Select Operational Period
+                    <option value="" disabled>
+                        {loading ? 'Loading...' : 'Select Operational Period'}
                     </option>
-                    {/* {incidentData.map((incident) => (
-                        <option key={incident.id} value={incident.id}>
-                            {incident.name}
+                    {operationalPeriodData.map((period) => (
+                        <option key={period.id} value={period.id}>
+                            {period.date_from} - {period.date_to}
                         </option>
-                    ))} */}
+                    ))}
                 </select>
             </div>
             <form
@@ -126,14 +218,56 @@ export default function Input() {
                         <tr>
                             <td className="px-4 py-2" colSpan={10}>
                                 <textarea
-                                    // name="situation_summary"
-                                    // value={formData.situation_summary}
+                                    name="special_instructions"
+                                    value={formData.special_instructions}
                                     className="w-full px-3 py-2 border rounded-md"
                                     rows="7"
                                     cols="50"
-                                    // onChange={handleChange}
+                                    onChange={handleChange}
                                     required
                                 />
+                            </td>
+                        </tr>
+
+                        {/* Prepared by */}
+                        <tr>
+                            <td className="px-4 py-2 font-bold">
+                                Prepared by:
+                            </td>
+                        </tr>
+                        <tr>
+                            <td className="px-4 py-2">
+                                {/* Select untuk Communication Unit Leader */}
+                                <select
+                                    name="communication_unit_leader_id"
+                                    className="flex-1 block w-[400px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
+                                    value={formData.communication_unit_leader_id || ""}
+                                    onChange={(e) => setFormData(prev => ({
+                                        ...prev,
+                                        communication_unit_leader_id: e.target.value
+                                    }))}
+                                    required
+                                >
+                                    <option value="" disabled>
+                                        Select Communcation Unit Leader
+                                    </option>
+                                    {CULeaderData.map(chief => (
+                                        <option key={chief.id} value={chief.id}>
+                                            {chief.name}
+                                        </option>
+                                    ))}
+                                </select>
+                                <input
+                                    type="checkbox"
+                                    name="is_prepared" // Nama field berbeda
+                                    checked={formData.is_prepared || false}
+                                    onChange={(e) => setFormData(prev => ({
+                                        ...prev,
+                                        is_prepared: e.target.checked
+                                    }))}
+                                    className="mr-2"
+                                />
+                                Signature
                             </td>
                         </tr>
 
---
+++ components/IAP/Ics205/RadioChannel.jsx
@@ -0,0 +1,149 @@
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+export default function RadioChannel({
+    rowsRadios,
+    onAddRow,
+    onRemoveRow,
+    onChangeRow,
+}) {
+
+    const handleRadioChange = (index, e) => {
+        const { name, value } = e.target;
+        onChangeRow(index, { [name]: value });
+    };
+
+    return (
+        <div>
+            <table className="w-full table-auto">
+                <thead>
+                    <tr>
+                        <th className="px-4 py-2 border">Channel Number</th>
+                        <th className="px-4 py-2 border">Channel Name</th>
+                        <th className="px-4 py-2 border">Radio Frequency</th>
+                        <th className="px-4 py-2 border">Mode</th>
+                        <th className="px-4 py-2 border">Function</th>
+                        <th className="px-4 py-2 border">Assignment</th>
+                        <th className="px-4 py-2 border">Remarks</th>
+                        <th className="border px-4 py-2 w-[8%]"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {rowsRadios.map((row, index) => (
+                        <tr key={index}>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='channel_number'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.channel_number}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='channel_name'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.channel_name}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='frequency'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.frequency}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <select
+                                    name='mode'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.mode}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    required
+                                >
+                                    <option value="" disabled>Select Mode</option>
+                                    <option value="Analog">Analog</option>
+                                    <option value="Digital">Digital</option>
+                                    <option value="Mixed">Mixed</option>
+                                </select>
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='functions'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.functions}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='assignment'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.assignment}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="px-4 py-2 border">
+                                <input
+                                    type="text"
+                                    name='remarks'
+                                    className="w-full px-2 py-1 border rounded"
+                                    value={row.remarks}
+                                    onChange={(e) => handleRadioChange(index, e)}
+                                    placeholder="Enter details"
+                                    required
+                                />
+                            </td>
+                            <td className="border px-4 py-2 text-center">
+                                {index === 0 ? (
+                                    <button
+                                        type='button'
+                                        onClick={onAddRow}
+                                        className="bg-[#548C2F] hover:bg-green-700 text-white font-bold w-10 h-10 flex items-center justify-center rounded"
+                                    >
+                                        +
+                                    </button>
+                                ) : (
+                                    <div className="flex flex-row gap-2">
+                                        <button
+                                            type='button'
+                                            onClick={onAddRow}
+                                            className="bg-[#548C2F] hover:bg-green-700 text-white font-bold w-10 h-10 flex items-center justify-center rounded"
+                                        >
+                                            +
+                                        </button>
+                                        <button
+                                            type='button'
+                                            onClick={() => onRemoveRow(index)}
+                                            className="bg-[#880D1E] hover:bg-red-700 text-white font-bold w-10 h-10 flex items-center justify-center rounded"
+                                        >
+                                            -
+                                        </button>
+                                    </div>
+                                )}
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+}
