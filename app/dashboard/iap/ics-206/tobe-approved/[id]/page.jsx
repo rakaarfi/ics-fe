407a27d39ddca87954e7192bb19f1b022fa70e5a
rakaarfi <rakaarfi@gmail.com>
2025-02-01 22:57:20 +0700

    add new route

--- app/dashboard/iap/ics-206/tobe-approved/[id]/page.jsx
+++ app/dashboard/iap/ics-206/tobe-approved/[id]/page.jsx
@@ -1,10 +1,10 @@
-import Detail from '@/components/IAP/Ics206/Detail'
+import ToBeApproved from '@/components/IAP/Ics206/ToBeApproved'
 import React from 'react'
 
 export default function page() {
     return (
         <div className="lg:ml-[17rem] ml-[9rem] lg:my-0 my-24 mx-2">
-            <Detail />
+            <ToBeApproved />
         </div>
     )
 }
