6c94824a5924de7f11197c05c394f5a092cafb5b
rakaarfi <rakaarfi@gmail.com>
2025-02-02 22:02:13 +0700

    change title

--- components/IAP/Ics208/Detail.jsx
+++ components/IAP/Ics208/Detail.jsx
@@ -191,7 +191,7 @@ export default function Detail() {
     }, []);
 
     return (
-        <FormContainer title="ICS 208 Safety Message/Plan Detail" >
+        <FormContainer title="ICS 208 - Safety Message/Plan Detail" >
             <div className="mb-4 flex flex-row">
                 <select
                     className="flex-1 block w-[400px] rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline focus:outline-2 focus:-outline-offset-2 focus:outline-[#55c0b8] sm:text-sm/6"
