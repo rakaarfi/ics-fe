eaa0c1a1171b401811f9e15a0965e3ba84277f95
rakaarfi <rakaarfi@gmail.com>
2025-02-04 22:32:20 +0700

    change link

--- components/IAP/Ics203/Preview.jsx
+++ components/IAP/Ics203/Preview.jsx
@@ -145,7 +145,7 @@ export default function Preview({
         const fetchPreparationData = async () => {
             try {
                 const response = await axios.get(
-                    `http://127.0.0.1:8000/ics-206/preparation/read-by-ics-206-id/${id}`
+                    `http://127.0.0.1:8000/ics-203/preparation/read-by-ics-203-id/${id}`
                 );
                 if (response.data.length > 0) {
                     setPreparationData(response.data);
