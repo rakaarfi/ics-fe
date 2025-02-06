a6da344eb2dd46ea6fd0cc72cd311a4a5216c2db
rakaarfi <rakaarfi@gmail.com>
2025-02-06 11:09:45 +0700

    modify

---
+++ app/dashboard/iap/ics-205/detail/[id]/page.jsx
@@ -0,0 +1,10 @@
+import Detail from '@/components/IAP/Ics205/Detail'
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className="lg:ml-[17rem] ml-[9rem] lg:my-0 my-24 mx-2">
+            <Detail />
+        </div>
+    )
+}
