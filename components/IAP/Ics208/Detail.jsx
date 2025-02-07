91f28cb316b2fcf22381df7063424aeb859d0c87
rakaarfi <rakaarfi@gmail.com>
2025-02-07 14:36:59 +0700

    add if condition for preparation

--- components/IAP/Ics208/Detail.jsx
+++ components/IAP/Ics208/Detail.jsx
@@ -150,7 +150,11 @@ export default function Detail() {
                 time_prepared: now.format('HH:mm'),
                 is_prepared: formData.is_prepared,
             };
-            await axios.put(`http://127.0.0.1:8000/ics-208/preparation/update/${preparationID}`, preparedPayload);
+            if (preparationID) {
+                await axios.put(`http://127.0.0.1:8000/ics-208/preparation/update/${preparationID}`, preparedPayload);
+            } else {
+                await axios.post('http://127.0.0.1:8000/ics-208/preparation/create', preparedPayload);
+            }
 
             alert('Data submitted successfully!');
         } catch (error) {
