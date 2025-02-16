59d732b6a37bb917413cde8da55d30d3c075df51
rakaarfi <rakaarfi@gmail.com>
2025-02-16 17:49:29 +0700

    comment darkmode toggle

--- app/layout.jsx
+++ app/layout.jsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <DarkModeToggle />
+        {/* <DarkModeToggle /> */}
         {children}
       </body>
     </html>
