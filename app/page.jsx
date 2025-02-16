2cdec7c4a110c3ae663935a8b932b7c5cc35a189
rakaarfi <rakaarfi@gmail.com>
2025-02-16 19:05:45 +0700

    modify

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,10 @@
 import LoginPage from "@/components/LoginPage";
 import IMTRosterChart from "@/components/ImtRoster/Chart";
-import Chart from "@/components/TestLagi";
 
 export default function Home() {
   return (
     <>
       {/* <LoginPage /> */}
-      {/* <Chart /> */}
     </>
   );
 }
