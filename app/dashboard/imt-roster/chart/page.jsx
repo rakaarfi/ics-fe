92fd73bd422316870679277483a5804466b2abfe
rakaarfi <rakaarfi@gmail.com>
2025-02-21 01:01:26 +0700

    remove margin from dashboard page

--- app/dashboard/imt-roster/chart/page.jsx
+++ app/dashboard/imt-roster/chart/page.jsx
@@ -4,9 +4,9 @@ import React from 'react'
 
 export default function page() {
     return (
-        <div className="lg:ml-[11rem] ml-[9rem] lg:my-0 my-24">
+        <>
             <ChartNameOnly />
             {/* <Chart /> */}
-        </div>
+        </>
     )
 }
