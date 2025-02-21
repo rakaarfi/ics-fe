e0257fbdb2970fa99810493ffe3eace544a52493
rakaarfi <rakaarfi@gmail.com>
2025-02-22 00:03:09 +0700

    remove log

--- components/IAP/Ics203/Preview.jsx
+++ components/IAP/Ics203/Preview.jsx
@@ -173,7 +173,6 @@ export default function Preview({
         try {
             const response = await fetchData('planning-section/resources-unit-leader');
             setRULeaderData(response);
-            console.log("Resources Unit Leader Data:", response);
         } catch (error) {
             console.error('Error fetching Resources Unit Leader data:', error);
             setError('Failed to fetch Resources Unit Leader data');
