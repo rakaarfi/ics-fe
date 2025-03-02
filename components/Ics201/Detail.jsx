80867b6e1432ca80d62723f743f3bd10882d12f8
rakaarfi <rakaarfi@gmail.com>
2025-03-02 14:48:54 +0700

    modify maxwidth chart

--- components/Ics201/Detail.jsx
+++ components/Ics201/Detail.jsx
@@ -611,7 +611,7 @@ export default function Detail() {
                                     className="border border-gray-300 rounded-md p-3"
                                     style={{
                                         maxHeight: '800px',
-                                        maxWidth: '1300px',
+                                        maxWidth: '1150px',
                                         overflowX: 'scroll',
                                         position: 'relative'
                                     }}
--- components/Ics201/Input.jsx
+++ components/Ics201/Input.jsx
@@ -386,7 +386,7 @@ export default function Input() {
                                     className="border border-gray-300 rounded-md p-3"
                                     style={{
                                         maxHeight: '800px',
-                                        maxWidth: '1300px',
+                                        maxWidth: '1150px',
                                         overflowX: 'scroll',
                                         position: 'relative'
                                     }}
